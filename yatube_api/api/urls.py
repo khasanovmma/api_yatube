ca35f82b12c7320f3fd3f81c8680df1468090504
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-04-07 21:47:31 +0500

    feat: implement api token auth

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework.authtoken import views as rest_framework_views
+
+
+urlpatterns = [
+    path("api-token-auth/", rest_framework_views.obtain_auth_token),
+]

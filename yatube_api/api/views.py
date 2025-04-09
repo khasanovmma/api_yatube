5aa17791da3dad04b051a437b554c0455862c6ce
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-04-09 07:37:11 +0500

    refact: remove unused imported modules

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
-from rest_framework import status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from posts.models import Post

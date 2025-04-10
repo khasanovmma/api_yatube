0054562a1c54e58a34b9d3af34a5c4864ca6ec76
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-04-10 10:00:54 +0500

    refact: update api url path and api views

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,17 +2,33 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken import views as rest_framework_views
 
-from .views import CommentDetailView, PostViewSet, GroupViewSet
+from .views import CommentViewSet, PostViewSet, GroupViewSet
 
 
 v1_router = routers.SimpleRouter()
 v1_router.register("posts", PostViewSet, basename="post")
 v1_router.register("groups", GroupViewSet, basename="groups")
+v1_comment_urls = [
+    path(
+        "v1/posts/<int:post_id>/comments/",
+        CommentViewSet.as_view({"get": "list", "post": "create"}),
+        name="comment-list",
+    ),
+    path(
+        "v1/posts/<int:post_id>/comments/<int:pk>/",
+        CommentViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="comment-detail",
+    ),
+]
 urlpatterns = [
     path("v1/api-token-auth/", rest_framework_views.obtain_auth_token),
     path("v1/", include(v1_router.urls)),
-    path(
-        "v1/posts/<int:post_id>/comments/<int:comment_id>/",
-        CommentDetailView.as_view(),
-    ),
+    *v1_comment_urls,
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,17 +1,9 @@
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
-
-
-from posts.models import Group, Post, Comment
+from posts.models import Group, Post
 from api.permissions import IsAuthorOrReadOnly
-from api.serializers import (
-    GroupSerializer,
-    PostSerializer,
-    CommentSerializer,
-)
+from api.serializers import GroupSerializer, PostSerializer, CommentSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -22,62 +14,21 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    @action(detail=True, methods=["GET", "POST"])
-    def comments(self, request, *args, **kwargs):
-        post = self.get_object()
-        if request.method == "GET":
-            serializer = CommentSerializer(post.comments.all(), many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user, post=post)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
-
-    def create(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-
-class CommentDetailView(RetrieveUpdateDestroyAPIView):
-    serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
     def get_queryset(self):
-        return Comment.objects.filter(post_id=self.kwargs["post_id"])
+        post = get_object_or_404(Post, pk=self.kwargs["post_id"])
+        return post.comments.all()
 
-    def get_object(self):
-        return self.get_queryset().filter(pk=self.kwargs["comment_id"]).first()
-
-    def check_author_permission(self, obj):
-        if obj is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        if self.request.user != obj.author:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        return None
-
-    def put(self, request, *args, **kwargs):
-        obj = self.get_object()
-        permission_error = self.check_author_permission(obj)
-        if permission_error:
-            return permission_error
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        obj = self.get_object()
-        permission_error = self.check_author_permission(obj)
-        if permission_error:
-            return permission_error
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        obj = self.get_object()
-        permission_error = self.check_author_permission(obj)
-        if permission_error:
-            return permission_error
-        return self.destroy(request, *args, **kwargs)
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs["post_id"])
+        serializer.save(author=self.request.user, post=post)

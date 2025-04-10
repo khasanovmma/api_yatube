668702037e5ef21d63c11c5b1100f377e359bfaf
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-04-10 09:19:20 +0500

    refact: update api url path

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,14 +5,14 @@ from rest_framework.authtoken import views as rest_framework_views
 from .views import CommentDetailView, PostViewSet, GroupViewSet
 
 
-router = routers.SimpleRouter()
-router.register("posts", PostViewSet)
-router.register("groups", GroupViewSet)
+v1_router = routers.SimpleRouter()
+v1_router.register("posts", PostViewSet, basename="post")
+v1_router.register("groups", GroupViewSet, basename="groups")
 urlpatterns = [
     path("v1/api-token-auth/", rest_framework_views.obtain_auth_token),
+    path("v1/", include(v1_router.urls)),
     path(
         "v1/posts/<int:post_id>/comments/<int:comment_id>/",
-        CommentDetailView.as_view()
+        CommentDetailView.as_view(),
     ),
-    path("v1/", include(router.urls)),
 ]

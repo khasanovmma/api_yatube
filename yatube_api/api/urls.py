338c9b3b92144839524c54df4d3105e87766d8f6
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-04-11 09:15:46 +0500

    refact: update api router

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,27 +8,11 @@ from .views import CommentViewSet, PostViewSet, GroupViewSet
 v1_router = routers.SimpleRouter()
 v1_router.register("posts", PostViewSet, basename="post")
 v1_router.register("groups", GroupViewSet, basename="groups")
-v1_comment_urls = [
-    path(
-        "v1/posts/<int:post_id>/comments/",
-        CommentViewSet.as_view({"get": "list", "post": "create"}),
-        name="comment-list",
-    ),
-    path(
-        "v1/posts/<int:post_id>/comments/<int:pk>/",
-        CommentViewSet.as_view(
-            {
-                "get": "retrieve",
-                "put": "update",
-                "patch": "partial_update",
-                "delete": "destroy",
-            }
-        ),
-        name="comment-detail",
-    ),
-]
+v1_router.register(
+    r"posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comment"
+)
+
 urlpatterns = [
     path("v1/api-token-auth/", rest_framework_views.obtain_auth_token),
     path("v1/", include(v1_router.urls)),
-    *v1_comment_urls,
 ]
